0e8acb112487434cd0524a9835c11f3899047846
gongbugi <rrobi1492@gmail.com>
2024-11-17 10:37:16 +0900

    로그인, 회원가입, 계정찾기

---
+++ DominoPj/src/dominoLogin/User.java
@@ -0,0 +1,14 @@
+package dominoLogin;
+
+import java.util.Scanner;
+
+public class User {
+	String id;
+	String pw;
+	
+	public void read(Scanner scan) {
+		id = scan.next();
+		pw = scan.next();
+	}
+
+}
